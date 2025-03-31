bf558bc371142fe72b2e6e7449bce82da1621e5e
dana gonzalez <dana.gonzalezdg1999@gmail.com>
2025-03-31 16:24:53 -0300

    punteros

--- tp1_1.c
+++ tp1_1.c
@@ -3,5 +3,24 @@
 
 void main()
 {
-    printf("hola mundo");
+    printf("Hola mundo\n");
+
+    // Declaracion de la variable y el puntero
+    int num = 100;
+    int *punt = &num;
+
+    // Imprimir el contenido del puntero
+    printf("1) El contenido del puntero: %d\n", *punt);
+
+    // Imprimir la direccion de memoria almacenada por el puntero
+    printf("2) La direccion de memoria almacenada por el puntero: %p\n", punt);
+
+    // Imprimir la direccion de memoria de la variable
+    printf("3) La direccion de memoria de la variable: %p\n", &num);
+
+    // Imprimir la direccion de memoria del puntero
+    printf("4) La direccion de memoria del puntero: %p\n", &punt);
+
+    // Imprimir el tamaño de memoria utilizado por la variable
+    // printf("5) El tamanio de memoria utilizado por la variable: %zu bytes\n", sizeof(num));
 }
